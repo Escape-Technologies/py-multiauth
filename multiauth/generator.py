f96b70abc9d3a8017ebbb669d215ef99ad8b68a1
Antoine Carossio <antoine.carossio@me.com>
2022-07-22 20:24:14 +0200

    feat: add curl to escaperc

--- multiauth/generator.py
+++ multiauth/generator.py
@@ -1,12 +1,24 @@
 """Generate authrc from -H or cURL."""
 
+import base64
+import json
+from typing import cast
+from urllib.parse import parse_qs
+
+import graphql
+
+from multiauth.config import PY_MULTIAUTH_LOGGER as logger
+from multiauth.types.http import HTTPMethod
 from multiauth.types.main import AuthTech
+from multiauth.utils import uncurl
+
+POTENTIAL_FIELD_NAME = ['token']
 
 
-def serialize_headers_to_manual(headers: dict[str, str] | list[str] | str) -> tuple[dict, dict]:
-    """Serialize raw headers in "manual" auth format."""
+def deserialize_headers(headers: dict[str, str] | list[str] | str) -> dict[str, str]:
+    """Convert headers to standard format."""
 
-    headers_dict: dict[str, str] = {}
+    res: dict[str, str] = {}
 
     if isinstance(headers, str):
         headers = [headers]
@@ -15,10 +27,17 @@ def serialize_headers_to_manual(headers: dict[str, str] | list[str] | str) -> tu
 
         for header in headers:
             header_split = header.split(':', 1)
-            headers_dict[header_split[0].strip()] = header_split[1].strip()
+            res[header_split[0].strip()] = header_split[1].strip()
 
-    elif isinstance(headers, dict):
-        headers_dict = headers
+        return res
+
+    return headers
+
+
+def raw_headers_to_manual(headers: dict[str, str] | list[str] | str) -> tuple[dict, dict]:
+    """Serialize raw headers in "manual" auth format."""
+
+    headers_dict = deserialize_headers(headers)
 
     auth_name = 'manual_headers'
 
@@ -36,3 +55,183 @@ def serialize_headers_to_manual(headers: dict[str, str] | list[str] | str) -> tu
     }
 
     return auths, users
+
+
+def urlencoded_to_json(data: str | None) -> str | None:
+    """This function transforms data in application/x-www-form-urlencoded to json data."""
+
+    if data is None:
+        return None
+
+    new_form = parse_qs(data)
+    json_data: dict = {}
+    for name, value in new_form.items():
+        if len(value) == 1:
+            json_data[name] = value[0]
+        else:
+            json_data[name] = value
+
+    return json.dumps(json_data)
+
+
+def rest_fill(rest_document: dict, url: str, method: HTTPMethod, headers: dict[str, str]) -> dict:
+    """This function fills the rest file."""
+
+    # The JSON schema for every authentication scheme
+    jsonschema: dict = {'users': {'user1': {'auth': 'schema1'}}, 'auth': {'schema1': {}}}
+
+    jsonschema['users']['user1'].update(rest_document)
+    jsonschema['auth']['schema1']['tech'] = 'rest'
+    jsonschema['auth']['schema1']['url'] = url
+    jsonschema['auth']['schema1']['method'] = method
+    jsonschema['auth']['schema1']['options'] = {'headers': headers}
+
+    return jsonschema
+
+
+def graphql_fill(graphql_document: dict, url: str, method: HTTPMethod, headers: dict[str, str], variables: dict = None) -> dict:
+    """This function fills the graphql escaperc file."""
+
+    # The JSON schema for every authentication scheme
+    jsonschema: dict = {'users': {'user1': {'auth': 'schema1'}}, 'auth': {'schema1': {}}}
+
+    jsonschema['auth']['schema1']['tech'] = 'graphql'
+
+    jsonschema['auth']['schema1']['url'] = url
+    jsonschema['auth']['schema1']['method'] = method
+
+    # Now we need to start finding the information about the mutation
+    mutation_name = graphql_document['definitions'][0]['selection_set']['selections'][0]['name']['value']
+    jsonschema['auth']['schema1']['mutation_name'] = mutation_name
+
+    # Now we need to get the user information
+    credentials: dict = {}
+    if variables and graphql_document['definitions'][0]['variable_definitions']:
+        for variable in graphql_document['definitions'][0]['variable_definitions']:
+            variable_name = variable['variable']['name']['value']
+            if variable_name in variables:
+                credentials[variable_name] = variables[variable_name]
+
+    else:
+        arguments = graphql_document['definitions'][0]['selection_set']['selections'][0]['arguments']
+        if isinstance(arguments, list):
+            for argument in arguments:
+                if argument['value'].get('fields') is None:
+                    credentials[argument['name']['value']] = argument['value']['value']
+                else:
+                    credentials[argument['name']['value']] = {}
+                    for input_object_field in argument['value']['fields']:
+                        credentials[argument['name']['value']][input_object_field['name']['value']] = input_object_field['value']['value']
+
+    # Now regarding the field
+    mutation_fields = graphql_document['definitions'][0]['selection_set']['selections'][0]['selection_set']['selections']
+    for field in mutation_fields:
+        if field['name']['value'].lower() in POTENTIAL_FIELD_NAME:
+            jsonschema['auth']['schema1']['mutation_field'] = field['name']['value']
+            break
+
+    jsonschema['users']['user1'].update(credentials)
+
+    jsonschema['auth']['schema1']['options'] = {}
+    jsonschema['auth']['schema1']['options']['operation'] = graphql_document['definitions'][0]['operation']
+
+    if headers:
+        jsonschema['auth']['schema1']['options']['headers'] = headers
+
+    return jsonschema
+
+
+#pylint: disable=too-many-branches, too-many-statements
+def curl_to_escaperc(curl: str) -> dict | None:
+    """This function transforms the curl request to an escaperc file."""
+
+    # First we uncurl
+    parsed_content = uncurl(curl)
+
+    # First thing we have to check if in the headers, there is a basic authentication or a token already
+    for header_key, header_value in parsed_content.headers.items():
+        if 'authorization' in header_key.lower():
+            if 'basic' in header_value.lower():
+                # Then the type of authentification is basic
+                decoded_value: str = cast(str, base64.b64decode(header_value.split(' ')[1]))
+                username, password = decoded_value.split(':', 1)
+
+                # The JSON schema for every authentication scheme
+                jsonschema: dict = {
+                    'users': {
+                        'user_basic': {
+                            'auth': 'auth_basic',
+                            'username': username,
+                            'password': password,
+                        }
+                    },
+                    'auth': {
+                        'auth_basic': AuthTech.BASIC
+                    },
+                }
+
+                headers: dict = {}
+                for key, value in parsed_content.headers.items():
+                    if 'authorization' not in key.lower():
+                        headers[key] = value
+
+                if headers:
+                    jsonschema['auth']['auth_basic']['options'] = {'headers': headers}
+
+                return jsonschema
+
+    # First the easiest thing to do is to to check if the data that we have is graphql data
+    query: dict = {}
+    escaperc: dict = {}
+
+    try:
+        if parsed_content.data is not None:
+            query = json.loads(parsed_content.data)
+    except Exception:
+        logger.debug('error has accured while loading the JSON file')
+
+    if query:
+        if query.get('query') is not None:
+            logger.info('Type of authetication detected: GraphQL')
+            graphql_tree = graphql.parse(query['query']).to_dict()
+            if query.get('variables') is not None:
+                escaperc = graphql_fill(graphql_tree, parsed_content.url, parsed_content.method, parsed_content.headers, query['variables'])
+            else:
+                escaperc = graphql_fill(graphql_tree, parsed_content.url, parsed_content.method, parsed_content.headers)
+
+        else:
+            # Then the authentication type is rest
+            logger.info('Type of authetication detected: REST')
+            escaperc = rest_fill(query, parsed_content.url, parsed_content.method, parsed_content.headers)
+
+    else:
+        # If we reached this else then the request sent is not being sent as application/json and is being sent as something else
+        # Most probably it is an application/x-www-form-urlencoded format
+        # Convert this format to json
+        json_data = urlencoded_to_json(parsed_content.data)
+
+        new_query: dict = {}
+        try:
+            if json_data is not None:
+                new_query = json.loads(json_data)
+        except Exception:
+            logger.debug('error has accured while loading the JSON file')
+
+        if new_query:
+            if new_query.get('query') is not None:
+                logger.info('Type of authetication detected: GraphQL')
+                graphql_tree = graphql.parse(new_query['query']).to_dict()
+                if new_query.get('variables') is not None:
+                    escaperc = graphql_fill(graphql_tree, parsed_content.url, parsed_content.method, parsed_content.headers, new_query['variables'])
+                else:
+                    escaperc = graphql_fill(graphql_tree, parsed_content.url, parsed_content.method, parsed_content.headers)
+
+            else:
+                # Then the authentication type is rest
+                logger.info('Type of authetication detected: REST')
+                escaperc = rest_fill(new_query, parsed_content.url, parsed_content.method, parsed_content.headers)
+
+        else:
+            return None
+
+    return escaperc
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,10 +1,204 @@
-#pylint: disable=redefined-outer-name, unused-import
+#pylint: disable=redefined-outer-name, unused-import, line-too-long, invalid-name
 
 """Test generator."""
+import pytest
 
-from multiauth.generator import serialize_headers_to_manual
+from multiauth.generator import curl_to_escaperc, raw_headers_to_manual
 
 from .providers.test_manual_auth import auth, users_one_header, users_two_headers  # noqa
+
+
+@pytest.fixture
+def graphql_curl_with_input_object_and_no_var() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://apismxu6skxg-backend.functions.fnc.fr-par.scw.cloud/graphql' \
+--header 'Content-Type: application/json' \
+--data-raw '{"query":"mutation {\r\n    login(userLoginInput: {email: \"karim.rustom@escape.tech\", password: \"frenchfries\"}){\r\n        token\r\n    }\r\n}","variables":{}}'"""
+
+
+@pytest.fixture
+def graphql_curl_with_input_object_and_var() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://apismxu6skxg-backend.functions.fnc.fr-par.scw.cloud/graphql' \
+--header 'Content-Type: application/json' \
+--data-raw '{"query":"mutation ($Login: UserLoginInput!){\r\n    login(userLoginInput: $Login){\r\n        token\r\n    }\r\n}","variables":{"Login":{"email":"karim.rustom@escape.tech","password":"frenchfries"}}}'"""
+
+
+@pytest.fixture
+def graphql_curl_with_normal_graphql_query() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://www.terrang.fr/graphql' \
+--header 'Content-Type: application/json' \
+--data-raw '{"query":"mutation {\r\n    authenticateUser(username: \"jw.anon.5172@gmail.com\", password: \"Wj7UxfFTyzgPVM\"){\r\n        success\r\n    }\r\n}","variables":{"Login":{"email":"karim.rustom@escape.tech","password":"frenchfries"}}}'"""
+
+
+@pytest.fixture
+def rest_curl() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://auth.console.fauna.com/login' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email":"jw.anon.5172@gmail.com",
+    "password":"Wj7UxfFTyzgPVM@"
+}'"""
+
+
+@pytest.fixture
+def rest_curl_not_json() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://auth.console.fauna.com/login' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'email=jw.anon.5172@gmail.com' \
+--data-urlencode 'password=Wj7UxfFTyzgPVM@'"""
+
+
+@pytest.fixture
+def curl_no_data() -> str:
+    """Test auth schema."""
+    return r"""curl --location --request POST 'https://auth.console.fauna.com/login' \
+--header 'Content-Type: application/x-www-form-urlencoded'"""
+
+
+@pytest.fixture
+def graphql_curl_with_input_object_and_no_var_response() -> dict:
+    """Curl Response."""
+    return {
+        'users': {
+            'user1': {
+                'auth': 'schema1',
+                'userLoginInput': {
+                    'email': 'karim.rustom@escape.tech',
+                    'password': 'frenchfries'
+                }
+            }
+        },
+        'auth': {
+            'schema1': {
+                'tech': 'graphql',
+                'url': 'https://apismxu6skxg-backend.functions.fnc.fr-par.scw.cloud/graphql',
+                'method': 'POST',
+                'mutation_name': 'login',
+                'mutation_field': 'token',
+                'options': {
+                    'operation': 'mutation',
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            }
+        }
+    }
+
+
+@pytest.fixture
+def graphql_curl_with_input_object_and_var_response() -> dict:
+    """Curl Response."""
+    return {
+        'users': {
+            'user1': {
+                'auth': 'schema1',
+                'Login': {
+                    'email': 'karim.rustom@escape.tech',
+                    'password': 'frenchfries'
+                }
+            }
+        },
+        'auth': {
+            'schema1': {
+                'tech': 'graphql',
+                'url': 'https://apismxu6skxg-backend.functions.fnc.fr-par.scw.cloud/graphql',
+                'method': 'POST',
+                'mutation_name': 'login',
+                'mutation_field': 'token',
+                'options': {
+                    'operation': 'mutation',
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            }
+        }
+    }
+
+
+@pytest.fixture
+def graphql_curl_with_normal_graphql_query_response() -> dict:
+    """Curl Response."""
+    return {
+        'users': {
+            'user1': {
+                'auth': 'schema1',
+                'username': 'jw.anon.5172@gmail.com',
+                'password': 'Wj7UxfFTyzgPVM'
+            }
+        },
+        'auth': {
+            'schema1': {
+                'tech': 'graphql',
+                'url': 'https://www.terrang.fr/graphql',
+                'method': 'POST',
+                'mutation_name': 'authenticateUser',
+                'options': {
+                    'operation': 'mutation',
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            }
+        }
+    }
+
+
+@pytest.fixture
+def rest_curl_response() -> dict:
+    """Curl Response."""
+    return {
+        'users': {
+            'user1': {
+                'auth': 'schema1',
+                'email': 'jw.anon.5172@gmail.com',
+                'password': 'Wj7UxfFTyzgPVM@'
+            }
+        },
+        'auth': {
+            'schema1': {
+                'tech': 'rest',
+                'url': 'https://auth.console.fauna.com/login',
+                'method': 'POST',
+                'options': {
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            }
+        }
+    }
+
+
+@pytest.fixture
+def rest_curl_not_json_response() -> dict:
+    """Curl Response."""
+    return {
+        'users': {
+            'user1': {
+                'auth': 'schema1',
+                'email': 'jw.anon.5172@gmail.com',
+                'password': 'Wj7UxfFTyzgPVM@'
+            }
+        },
+        'auth': {
+            'schema1': {
+                'tech': 'rest',
+                'url': 'https://auth.console.fauna.com/login',
+                'method': 'POST',
+                'options': {
+                    'headers': {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }
+            }
+        }
+    }
 
 
 def test_serialize_headers(auth: dict, users_one_header: dict, users_two_headers: dict) -> None:
@@ -14,17 +208,55 @@ def test_serialize_headers(auth: dict, users_one_header: dict, users_two_headers
     headers_list = ['Authorization: Bearer 12345', 'Content-Type: application/json']
     headers_dict = {'Authorization': 'Bearer 12345', 'Content-Type': 'application/json'}
 
-    auths_str, users_str = serialize_headers_to_manual(headers_str)
+    auths_str, users_str = raw_headers_to_manual(headers_str)
 
     assert auths_str == auth
     assert users_str == users_one_header
 
-    auths_list, users_list = serialize_headers_to_manual(headers_list)
+    auths_list, users_list = raw_headers_to_manual(headers_list)
 
     assert auths_list == auth
     assert users_list == users_two_headers
 
-    auths_dict, users_dict = serialize_headers_to_manual(headers_dict)
+    auths_dict, users_dict = raw_headers_to_manual(headers_dict)
 
     assert auths_dict == auth
     assert users_dict == users_two_headers
+
+
+def test_graphql_curl_with_input_object_and_no_var(
+    graphql_curl_with_input_object_and_no_var: str, graphql_curl_with_input_object_and_no_var_response: dict
+) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(graphql_curl_with_input_object_and_no_var) == graphql_curl_with_input_object_and_no_var_response
+
+
+def test_graphql_curl_with_input_object_and_var(graphql_curl_with_input_object_and_var: str, graphql_curl_with_input_object_and_var_response: dict) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(graphql_curl_with_input_object_and_var) == graphql_curl_with_input_object_and_var_response
+
+
+def test_graphql_curl_with_normal_graphql_query(graphql_curl_with_normal_graphql_query: str, graphql_curl_with_normal_graphql_query_response: dict) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(graphql_curl_with_normal_graphql_query) == graphql_curl_with_normal_graphql_query_response
+
+
+def test_rest_curl(rest_curl: str, rest_curl_response: dict) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(rest_curl) == rest_curl_response
+
+
+def test_rest_curl_not_json(rest_curl_not_json: str, rest_curl_not_json_response: dict) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(rest_curl_not_json) == rest_curl_not_json_response
+
+
+def test_curl_no_data(curl_no_data: str) -> None:
+    """Function that tests if the curl to escaperc works."""
+
+    assert curl_to_escaperc(curl_no_data) is None
