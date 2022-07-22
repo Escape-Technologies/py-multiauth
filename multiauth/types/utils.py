f2679167dc93c15a590153c5a4dcc4f487248343
Antoine Carossio <antoine.carossio@me.com>
2022-07-22 20:06:01 +0200

    feat: add uncurl

---
+++ multiauth/types/utils.py
@@ -0,0 +1,23 @@
+"""Custom data used in utils."""
+
+from typing import NamedTuple
+
+from multiauth.types.http import HTTPMethod
+
+
+class Credentials(NamedTuple):
+
+    """This is the credentials class that are the credentials found in the curl."""
+    username: str
+    password: str
+
+
+class ParsedCurlContent(NamedTuple):
+
+    """This is the datatype which shows the curl command."""
+
+    method: HTTPMethod
+    url: str
+    data: str | None
+    headers: dict[str, str]
+    credentials: Credentials | None
