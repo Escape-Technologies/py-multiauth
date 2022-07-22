11feca24b51c24567d8952b8d9f91283e265e4c4
Antoine Carossio <antoine.carossio@me.com>
2022-07-22 15:58:05 +0200

    feat: add raw header support

--- multiauth/providers/manual.py
+++ multiauth/providers/manual.py
@@ -35,3 +35,40 @@ def manual_authenticator(user: User) -> AuthResponse:
     auth_response['tech'] = AuthTech.MANUAL
 
     return auth_response
+
+
+def serialize_headers_to_manual(headers: dict[str, str] | list[str] | str) -> tuple[dict, dict]:
+    """Serialize raw headers in "manual" auth format."""
+
+    headers_dict: dict[str, str] = {}
+
+    if isinstance(headers, str):
+        headers = [headers]
+
+    if isinstance(headers, list):
+
+        for header in headers:
+            header_split = header.split(':')
+            header_name = header_split[0].strip()
+            header_value = ':'.join(header_split[1:]).strip()
+            headers_dict[header_name] = header_value
+
+    elif isinstance(headers, dict):
+        headers_dict = headers
+
+    auth_name = 'manual_headers'
+
+    auths: dict = {
+        auth_name: {
+            'tech': AuthTech.MANUAL,
+        },
+    }
+
+    users: dict = {
+        'manual_user': {
+            'headers': headers_dict,
+            'auth': auth_name
+        },
+    }
+
+    return auths, users
--- tests/providers/test_manual_auth.py
+++ tests/providers/test_manual_auth.py
@@ -5,7 +5,7 @@
 import pytest
 
 from multiauth import MultiAuth
-from multiauth.providers.manual import manual_authenticator
+from multiauth.providers.manual import manual_authenticator, serialize_headers_to_manual
 
 
 @pytest.fixture
@@ -20,11 +20,11 @@ def auth() -> dict:
 
 
 @pytest.fixture
-def user_config_credentials() -> dict:
+def users_one_header() -> dict:
     """Return a fixture of users."""
 
     return {
-        'user_lambda': {
+        'manual_user': {
             'auth': 'manual_headers',
             'headers': {
                 'Authorization': 'Bearer 12345'
@@ -34,54 +34,75 @@ def user_config_credentials() -> dict:
 
 
 @pytest.fixture
-def user_config_headers() -> dict:
+def users_two_headers() -> dict:
     """Return a fixture of users."""
 
     return {
-        'user_lambda': {
+        'manual_user': {
             'auth': 'manual_headers',
             'headers': {
-                'Authorization': 'Bearer 12345'
+                'Authorization': 'Bearer 12345',
+                'Content-Type': 'application/json'
             }
         },
     }
 
 
-def test_manual_authentication_credentials(user_config_credentials: dict, auth: dict) -> None:
+def test_manual_authentication_headers(users_one_header: dict, auth: dict) -> None:
     """Test manual authentication."""
 
-    instance = MultiAuth(auth, user_config_credentials)
+    instance = MultiAuth(auth, users_one_header)
     instance.authenticate_users()
 
-    assert instance.headers['user_lambda']['Authorization'] == 'Bearer 12345'
+    assert instance.headers['manual_user']['Authorization'] == 'Bearer 12345'
 
-    headers, _ = instance.authenticate('user_lambda')
+    headers, _ = instance.authenticate('manual_user')
+
+    assert len(headers) == 1
     assert headers['Authorization'] == 'Bearer 12345'
 
 
-def test_manual_authentication_headers(user_config_headers: dict, auth: dict) -> None:
+def test_manual_authentication_credentials(users_two_headers: dict, auth: dict) -> None:
     """Test manual authentication."""
 
-    instance = MultiAuth(auth, user_config_headers)
+    instance = MultiAuth(auth, users_two_headers)
     instance.authenticate_users()
 
-    assert instance.headers['user_lambda']['Authorization'] == 'Bearer 12345'
+    assert instance.headers['manual_user']['Authorization'] == 'Bearer 12345'
 
-    headers, _ = instance.authenticate('user_lambda')
+    headers, _ = instance.authenticate('manual_user')
+
+    assert len(headers) == 2
     assert headers['Authorization'] == 'Bearer 12345'
+    assert headers['Content-Type'] == 'application/json'
 
 
-def test_manual_handler_credentials(user_config_credentials: dict, auth: dict) -> None:
+def test_manual_handler_headers(users_one_header: dict, auth: dict) -> None:
     """Test manual handler."""
 
-    auth_response = manual_authenticator(MultiAuth.serialize_users(auth, user_config_credentials)['user_lambda'])
+    auth_response = manual_authenticator(MultiAuth.serialize_users(auth, users_one_header)['manual_user'])
 
     assert auth_response['headers']['Authorization'] == 'Bearer 12345'
 
 
-def test_manual_handler_headers(user_config_headers: dict, auth: dict) -> None:
-    """Test manual handler."""
+def test_serialize_headers(auth: dict, users_one_header: dict, users_two_headers: dict) -> None:
+    """Test serialize_headers."""
 
-    auth_response = manual_authenticator(MultiAuth.serialize_users(auth, user_config_headers)['user_lambda'])
+    headers_str = 'Authorization: Bearer 12345'
+    headers_list = ['Authorization: Bearer 12345', 'Content-Type: application/json']
+    headers_dict = {'Authorization': 'Bearer 12345', 'Content-Type': 'application/json'}
 
-    assert auth_response['headers']['Authorization'] == 'Bearer 12345'
+    auths_str, users_str = serialize_headers_to_manual(headers_str)
+
+    assert auths_str == auth
+    assert users_str == users_one_header
+
+    auths_list, users_list = serialize_headers_to_manual(headers_list)
+
+    assert auths_list == auth
+    assert users_list == users_two_headers
+
+    auths_dict, users_dict = serialize_headers_to_manual(headers_dict)
+
+    assert auths_dict == auth
+    assert users_dict == users_two_headers
