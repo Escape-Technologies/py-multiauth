8c4a3830b9d88281ebb81bac0ee5a1431e1bb9d1
QuentinN42 <quentin@lieumont.fr>
2023-11-06 18:09:50 +0100

    chore: dev env
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- install-dev.sh
+++ install-dev.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-echo "---- Installing Python Poetry ----"
+
+echo -e "\n\n\n\n\n---- Installing Python Poetry ----"
 pip install -U pip
 pip install -U poetry
 poetry config virtualenvs.in-project true
 
-echo "---- Installing Python dependencies ----"
+echo -e "\n\n\n\n\n---- Installing Python dependencies ----"
 poetry install
 
-echo "\n\n\n\n\n---- Git hooks init (using mookme) ----"
+echo -e "\n\n\n\n\n---- Git hooks init (using mookme) ----"
 npm install
 npx mookme init --only-hook --skip-types-selection
 
-echo "\n\n\n\n\n---- Your working directory is all set :) ----"
+echo -e "\n\n\n\n\n---- Your working directory is all set :) ----"
