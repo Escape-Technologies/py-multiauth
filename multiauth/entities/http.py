5bb816d9cc267d9913c4c09f6bbd12fa35dd2d48
Maxence Lecanu <maxence@escape.tech>
2023-12-19 10:04:11 +0100

    feat: add http request entity and send_request procedure

--- multiauth/entities/http.py
+++ multiauth/entities/http.py
@@ -1,7 +1,8 @@
 """Multiauth types related to HTTP protocol."""
 
 import sys
-from enum import Enum, unique
+from dataclasses import dataclass, field
+from enum import Enum, StrEnum, unique
 
 if sys.version_info >= (3, 8):
     from typing import Literal  # pylint: disable=no-name-in-module
@@ -18,3 +19,24 @@ class Location(str, Enum):
 
     HEADERS = 'headers'
     URL = 'url'
+
+class HttpScheme(StrEnum):
+    HTTP = 'http'
+    HTTPS = 'https'
+
+JSONSerializable = dict | list | str | int | float | bool | None
+
+@dataclass
+class HttpRequest:
+    method: HTTPMethod
+    host: str
+    scheme: HttpScheme
+    path: str = '/'
+    headers: dict[str, str] = field(default_factory=dict)
+    username: str | None = None
+    password: str | None = None
+    json: JSONSerializable | None = None
+    data: str | None = None
+    query_parameters: dict[str, str] = field(default_factory=dict)
+    cookies: dict[str, str] = field(default_factory=dict)
+    timeout: int = 5
--- multiauth/providers/http.py
+++ multiauth/providers/http.py
@@ -1,11 +1,15 @@
 """Implementation of the Rest authentication schema."""
 
-from typing import Dict
+from typing import Any, Dict
+
+from requests import Response, request
 
 from multiauth.entities.errors import AuthenticationError
 from multiauth.entities.main import AuthResponse, AuthTech
 from multiauth.entities.providers.http import (
     AuthProvider,
+    AuthRequester,
+    Credentials,
 )
 from multiauth.manager import User
 from multiauth.providers.http_parser import parse_config
@@ -38,3 +42,31 @@ def http_authenticator(
 
     auth_config = parse_config(schema)
     return attach_auth(user, auth_config, proxy=proxy)
+
+
+def merge_data(base_data: Any, user_data: Any) -> Any:
+    # If the base data is a dict, and the user data is a dict, then we merge them
+    if isinstance(base_data, dict) and isinstance(user_data, dict):
+        return base_data | user_data
+
+    # If the base data is a list, and the user data is a list, then we merge them
+    if isinstance(base_data, list) and isinstance(user_data, list):
+        return base_data + user_data
+
+    if user_data is None:
+        return base_data
+
+    # In any other case, user_data prevails over base_data
+    return user_data
+
+def send_http_request(requester: AuthRequester, credential: Credentials) -> Response:
+    url = requester.url
+    method = requester.method
+
+    headers = requester.headers | credential.headers
+    cookies = requester.cookies | credential.cookies
+
+    data = merge_data(requester.body, credential.body)
+
+    return request(method, url, headers=headers, cookies=cookies, data=data)
+
