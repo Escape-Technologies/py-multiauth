2387eb561a53a453d0392ba6bd5f9696ee592ddb
Antoine Carossio <antoine.carossio@me.com>
2023-12-26 00:02:49 +0100

    feat: type OAuth Interfaces

--- multiauth/new/entities/oauth.py
+++ multiauth/new/entities/oauth.py
@@ -1,4 +1,12 @@
-from multiauth.new.entities.main import Credentials
+from pydantic import Field
+
+from multiauth.entities.providers.oauth import AuthOAuthClientMethod
+from multiauth.entities.providers.webdriver import SeleniumCommand
+from multiauth.new.entities.main import AuthProvider, AuthRequester, Credentials
+
+###########################
+#### OAuth2 Auth Code #####
+###########################
 
 
 class OAuth2AuthCodeCredentials(Credentials):
@@ -6,9 +14,47 @@ class OAuth2AuthCodeCredentials(Credentials):
     clientSecret: str
 
 
+class OAuth2AuthCodeAuthRequester(AuthRequester):
+    url: str = Field(description='The URL to request the authorization code')
+    tokenUrl: str = Field(description='The URL to exchange the authorization code for an access token')
+    redirectUrl: str = Field(description='The URL to redirect to after authorization')
+    clientMethod: AuthOAuthClientMethod = Field(
+        default=AuthOAuthClientMethod.BASIC,
+        description='The client authentication method for token endpoint.',
+    )
+    webDriverCommmands: list[SeleniumCommand] = Field(description='The commands to execute in the web driver')
+    scope: str | None = Field(default=None, description='The scope to request access to')
+    state: str | None = Field(default=None, description='The state is used to prevent CSRF attacks')
+    codeVerifier: str | None = Field(default=None, description='The code verifier used to generate the code challenge')
+
+
+class OAuth2AuthCodeAuthProvider(AuthProvider):
+    """This OAuth2 flow requires a Webdriver."""
+
+    requester: OAuth2AuthCodeAuthRequester
+
+
+###########################
+###### OAuth2 Client ######
+###########################
 class OAuth2ClientCredentials(Credentials):
     clientId: str
     clientSecret: str
+
+
+class OAuth2ClientAuthRequester(AuthRequester):
+    url: str = Field(description='The URL to request the authorization code')
+    tokenUrl: str = Field(description='The URL to exchange the authorization code for an access token')
+    scope: str | None = Field(default=None, description='The scope to request access to')
+
+
+class OAuth2ClientAuthProvider(AuthProvider):
+    requester: OAuth2ClientAuthRequester
+
+
+###########################
+##### OAuth2 Password #####
+###########################
 
 
 class OAuth2PasswordCredentials(Credentials):
@@ -18,5 +64,55 @@ class OAuth2PasswordCredentials(Credentials):
     password: str
 
 
+class OAuth2PasswordAuthRequester(AuthRequester):
+    url: str = Field(description='The URL to request the authorization code')
+    tokenUrl: str = Field(description='The URL to exchange the authorization code for an access token')
+    clientMethod: AuthOAuthClientMethod = Field(
+        default=AuthOAuthClientMethod.BASIC,
+        description='The client authentication method for token endpoint.',
+    )
+    scope: str | None = Field(default=None, description='The scope to request access to')
+
+
+class OAuth2PasswordAuthProvider(AuthProvider):
+    requester: OAuth2PasswordAuthRequester
+
+
+###########################
+##### OAuth2 Implicit #####
+###########################
+
+
 class OAuth2ImplicitCredentials(Credentials):
     clientId: str
+
+
+class OAuth2ImplicitAuthRequester(AuthRequester):
+    url: str = Field(description='The URL to request the authorization code')
+    redirectUrl: str = Field(description='The URL to redirect to after authorization')
+    webDriverCommmands: list[SeleniumCommand] = Field(description='The commands to execute in the web driver')
+    clientMethod: AuthOAuthClientMethod = Field(
+        default=AuthOAuthClientMethod.BASIC,
+        description='The client authentication method for token endpoint.',
+    )
+    scope: str | None = Field(default=None, description='The scope to request access to')
+    state: str | None = Field(default=None, description='The state is used to prevent CSRF attacks')
+
+
+class OAuth2ImplicitAuthProvider(AuthProvider):
+    """This OAuth2 flow requires a Webdriver."""
+
+    requester: OAuth2ImplicitAuthRequester
+
+
+###########################
+###### OAuth2 Refresh #####
+###########################
+
+
+class OAuth2RefreshCredentials(Credentials):
+    refreshToken: str
+
+
+class OAuth2RefreshProvider(AuthProvider):
+    pass
