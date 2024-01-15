aea094fc048858716952bce53da9b29f8d191ea7
Antoine Carossio <antoine.carossio@me.com>
2024-01-15 18:32:30 +0100

    feat: cognito user-pass

--- multiauth/lib/presets/cognito_userpass.py
+++ multiauth/lib/presets/cognito_userpass.py
@@ -1,3 +1,4 @@
+import enum
 from http import HTTPMethod
 from typing import Literal, Sequence
 
@@ -8,11 +9,41 @@ from multiauth.lib.http_core.entities import HTTPEncoding, HTTPHeader, HTTPLocat
 from multiauth.lib.injection import TokenInjection
 from multiauth.lib.presets.base import BasePreset, HTTPRequestParameters
 from multiauth.lib.presets.basic import BasicUserPreset
-from multiauth.lib.presets.cognito_base import AWSRegion
 from multiauth.lib.procedure import ProcedureConfiguration
 from multiauth.lib.runners.http import HTTPRunnerConfiguration, TokenExtraction
 from multiauth.lib.store.user import User, UserRefresh
 from multiauth.lib.store.variables import AuthenticationVariable
+
+###########################
+## Auth with cURL: https://stackoverflow.com/questions/58833462/aws-cognito-authentication-curl-call-generate-token-without-cli-no-clien
+###########################
+
+
+class AWSRegion(enum.StrEnum):
+    US_EAST_OHIO = 'us-east-2'
+    US_EAST_N_VIRGINIA = 'us-east-1'
+    US_WEST_N_CALIFORNIA = 'us-west-1'
+    US_WEST_OREGON = 'us-west-2'
+    AFRICA_CAPE_TOWN = 'af-south-1'
+    ASIA_PACIFIC_HONG_KONG = 'ap-east-1'
+    ASIA_PACIFIC_MUMBAI = 'ap-south-1'
+    ASIA_PACIFIC_OSAKA = 'ap-northeast-3'
+    ASIA_PACIFIC_SEOUL = 'ap-northeast-2'
+    ASIA_PACIFIC_SINGAPORE = 'ap-southeast-1'
+    ASIA_PACIFIC_SYDNEY = 'ap-southeast-2'
+    ASIA_PACIFIC_TOKYO = 'ap-northeast-1'
+    CANADA_CENTRAL = 'ca-central-1'
+    CHINA_BEIJING = 'cn-north-1'
+    CHINA_NINGXIA = 'cn-northwest-1'
+    EUROPE_FRANKFURT = 'eu-central-1'
+    EUROPE_IRELAND = 'eu-west-1'
+    EUROPE_LONDON = 'eu-west-2'
+    EUROPE_MILAN = 'eu-south-1'
+    EUROPE_PARIS = 'eu-west-3'
+    EUROPE_STOCKHOLM = 'eu-north-1'
+    MIDDLE_EAST_BAHRAIN = 'me-south-1'
+    SOUTH_AMERICA_SAO_PAULO = 'sa-east-1'
+
 
 ###########################
 ###### AWS Password ######
